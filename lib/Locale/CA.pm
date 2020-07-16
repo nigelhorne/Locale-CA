e32cf332f830f853fd3ec9bbf456eb7bbcafe43a
Nigel Horne <nigel.horne@nasa.gov>
2020-07-16 09:20:11 -0400

    Release 0.04

--- lib/Locale/CA.pm
+++ lib/Locale/CA.pm
@@ -10,11 +10,11 @@ Locale::CA - two letter codes for province identification in Canada and vice ver
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
