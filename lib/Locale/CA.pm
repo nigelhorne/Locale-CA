913ccd446fa528fe53d34d94f4145c6597bdf076
Nigel Horne <njh@bandsman.co.uk>
2024-12-17 08:20:53 -0500

    Release 0.07

--- lib/Locale/CA.pm
+++ lib/Locale/CA.pm
@@ -11,11 +11,11 @@ Locale::CA - two letter codes for province identification in Canada and vice ver
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 =head1 SYNOPSIS
 
